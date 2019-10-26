3af5bed8e72f082bec0cf6dd7e49ffeee7275af2
Old-Fritz <komarov.99@yandex.ru>
2019-10-26 11:39:18 +0300

    add official class

---
+++ be/app/model/official.py
@@ -0,0 +1,8 @@
+class Official:
+    def __init__(self, photo_url, name, position, region_name, income, declaration_url):
+        self.photo_url = photo_url
+        self.name = name
+        self.position = position
+        self.region_name = region_name
+        self.income = income
+        self.declaration_url = declaration_url
