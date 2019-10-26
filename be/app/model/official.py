ad8df8a5aa17d9b7f543d4b965e7dd5049703419
Nick <nickkarmatskikh@gmail.com>
2019-10-26 12:39:59 +0300

    add id for official

--- be/app/model/official.py
+++ be/app/model/official.py
@@ -1,5 +1,6 @@
 class Official:
-    def __init__(self, photo_url, name, position, region_name, income, declaration_url):
+    def __init__(self, id, photo_url, name, position, region_name, income, declaration_url):
+        self.id = id
         self.photo_url = photo_url
         self.name = name
         self.position = position
