26760db805e978c94654bc3acc7aa3fdd02299b4
Old-Fritz <komarov.99@yandex.ru>
2019-10-26 20:13:50 +0300

    add destribution algorythm

---
+++ be/app/service/distribution.py
@@ -0,0 +1,33 @@
+import random
+from app.model.item import Item
+
+
+def calculate_distribution(income, data):
+    # set limit
+    direction = random.random() - 0.5 > 0
+    if direction:
+        income = income * (random.random() + 1)
+    else:
+        income = income * (0.5 + random.random() * 0.5)
+
+    print(income)
+
+    out_items = []
+    # check levels
+    for level_index in data:
+        level = data[level_index]
+        if level[-1]['price'] >= income:
+            continue
+
+        item_index = random.randrange(0, len(level))
+        while level[item_index]['price'] >= income:
+            item_index = random.randrange(item_index + 1, len(level))
+
+        item = level[item_index]
+        item_count = int(income / item['price'])
+        full_price = item_count * item['price']
+        income -= full_price
+
+        out_items.append(Item(level_index, item['name'], item['unit'], item_count, full_price))
+
+    return out_items
