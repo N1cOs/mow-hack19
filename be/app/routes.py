a9ef8479ab3fdaac1f0c81d2c7b49450aa361131
Nick <nickkarmatskikh@gmail.com>
2019-10-26 21:18:48 +0300

    add validation for ids

--- be/app/routes.py
+++ be/app/routes.py
@@ -1,13 +1,15 @@
 import app.service.api as api
 import time
 import logging
+import re
 from app.app import app
 from flask import request
 from app.model.official import Official
 from random import randint
-from flask import jsonify, g
+from flask import jsonify, g, abort
 
 OFFICIALS = {}
+OPTIONS_REGEX = re.compile(r'^((\d+?(\s?[,\s]\s?))*?)(\d+)$')
 
 
 def init():
@@ -16,7 +18,7 @@ def init():
         while line != '':
             id, name, surname, patronymic, photo = line.split()
             name = f'{name} {surname} {patronymic}'
-            OFFICIALS[id] = Official(id, photo, name)
+            OFFICIALS[id] = Official(int(id), photo, name)
             line = file.readline()
 
 
@@ -25,13 +27,16 @@ def official():
     prh_ids = request.args.get('ids')
     if prh_ids is None:
         prh_ids = []
+    elif OPTIONS_REGEX.match(prh_ids.strip()):
+        prh_ids = prh_ids.split(',')
+        prh_ids = [int(i.strip()) for i in prh_ids]
     else:
-        prh_ids = list(prh_ids)
-    
+        abort(400)
+
     ids = [i for i in OFFICIALS if i not in prh_ids]
     id = ids[randint(0, len(ids) - 1)]
     official = OFFICIALS[id]
-    
+
     pos, region, income, year, declaration_url = api.get_official_info(id)
     official.position = pos
     official.regionName = region
@@ -40,7 +45,6 @@ def official():
     official.declarationUrl = declaration_url
 
     return jsonify(vars(official))
-
 
 
 @app.route('/item', methods=['GET'])
