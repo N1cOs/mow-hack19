d087a3fd1e396abbea5263ac082174ec5a73e17e
Nick <nickkarmatskikh@gmail.com>
2019-10-26 11:43:33 +0300

    add db configuration

--- be/app/config.py
+++ be/app/config.py
@@ -1,2 +1,12 @@
+from os import getenv
+
+
 class Config:
     DEBUG = True
+
+    DB_USER = getenv('DB_USER')
+    DB_PASSWORD = getenv('DB_PASSWORD')
+
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/items'
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
