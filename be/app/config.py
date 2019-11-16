31f13e3ecea3c4f8f6559c750f0d7cfc06a01db0
Nick <nickkarmatskikh@gmail.com>
2019-11-16 20:05:49 +0300

    app/app.py, app/config.py, requirements.txt: remove sqlalchemy support

--- be/app/config.py
+++ be/app/config.py
@@ -4,11 +4,4 @@ IS_DEV = True if getenv('APP_MODE') == 'DEV' else False
 
 
 class Config:
-    DEBUG = IS_DEV 
-
-    DB_USER = getenv('DB_USER')
-    DB_PASSWORD = getenv('DB_PASSWORD')
-
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/items'
-    SQLALCHEMY_ECHO = IS_DEV
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = IS_DEV
